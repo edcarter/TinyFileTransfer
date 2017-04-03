171ae3025b06190f2199d4960d1e61b5cdca4731
edcarter <edcarter@ualberta.ca>
2017-04-02 18:20:30 -0600

    padding and unpadding for cipher now implemented

--- src/Server.java
+++ src/Server.java
@@ -7,14 +7,15 @@ import java.util.Arrays;
  */
 public class Server {
     public static void main(String[] args) {
-        byte[] b = "GGGGGGGGGGGGGGGG".getBytes(StandardCharsets.UTF_8);
+        byte[] b = "GG".getBytes(StandardCharsets.UTF_8);
         long[] k = new long[]{1,2,3,4};
-        System.out.println(b.length);
         System.out.println("Before: "  + Arrays.toString(b));
         System.loadLibrary("tea");
-        TEA.encrypt(b, k);
-        System.out.println("After: " + Arrays.toString(b));
-        TEA.decrypt(b, k);
-        System.out.println("Finally: " + Arrays.toString(b));
+
+        byte[] encrypted = TEA.do_encrypt(b, k);
+
+        System.out.println("After: " + Arrays.toString(encrypted));
+        byte[] decrypted = TEA.do_decrypt(encrypted, k);
+        System.out.println("Finally: " + Arrays.toString(decrypted));
     }
 }
--- src/TEA.java
+++ src/TEA.java
@@ -2,6 +2,51 @@
  * Created by elias on 30/03/17.
  */
 public class TEA {
-    public static native void encrypt(byte[] bytes, long[] key);
-    public static native void decrypt(byte[] bytes, long[] key);
+    private static int blockSize = 16;
+
+    public static byte[] do_encrypt(byte[] bytes, long[] key) {
+        byte[] padded = pad(bytes);
+        encrypt(padded, key);
+        return padded;
+    }
+
+    public static byte[] do_decrypt(byte[] bytes, long[] key) {
+        decrypt(bytes, key);
+        byte[] unpadded = unpad(bytes);
+        return unpadded;
+    }
+
+    // pad bytes using ISO/IEC 9797-1 padding method 2
+    // add a bit '1' after message, and then pad 0's to get desired length
+    private static byte[] pad(byte[] bytes) {
+        int length = bytes.length;
+        int remainder = length % blockSize;
+        byte[] padded = bytes;
+        if (remainder != 0) {
+            int toPad = blockSize - remainder;
+            padded = new byte[length + toPad];
+            System.arraycopy(bytes, 0, padded, 0, length);
+            padded[length] = 8; // pad '10000000'
+            for (int i = 1; i < toPad; i++) {
+                padded[length + i] = 0; // pad '00000000'
+            }
+        }
+        return padded;
+    }
+
+    private static byte[] unpad(byte[] bytes) {
+        int index = bytes.length - 1;
+        for ( ; index > 0; index--) {
+            byte b = bytes[index];
+            if (b == 8) break; // we found the padding byte
+            if (b != 0) return bytes; // the message isn't padded
+        }
+        int unpaddedLength = index;
+        byte[] unpadded = new byte[unpaddedLength];
+        System.arraycopy(bytes, 0, unpadded, 0, unpaddedLength);
+        return unpadded;
+    }
+
+    private static native void encrypt(byte[] bytes, long[] key);
+    private static native void decrypt(byte[] bytes, long[] key);
 }
